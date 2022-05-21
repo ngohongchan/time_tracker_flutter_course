00431add4ed629f2cc1bce3c933735aebef72bf2
Chan Ngo <ngohongchan12a4@gmail.com>
2022-05-21 17:14:19 +0700

    Conenct firebase for flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCp1zFJIViCq3mYFiwK3pWjkvX-VBv5Bys", // Your apiKey
+      appId: "1:668536051557:web:70a42046ba45540d40338c", // Your appId
+      messagingSenderId: "668536051557", // Your messagingSenderId
+      projectId: "time-tracker-flutter-cou-83b95", // Your projectId
+    ),
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +27,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: SignInPage(),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("Error");
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SignInPage();
+          }
+  
+          return CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
